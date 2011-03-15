7d68c476af08b6e6f6af7f6410688f10387a685d
Hashem Gamal Ahmed <hashemgamal@gmail.com>
2011-03-15 20:56:40 +0400

    Added arabic translation for file-manager plugin.

---
+++ wolf/plugins/file_manager/i18n/ar-message.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Arabic language file for plugin file_manager
+ *
+ * @package Plugins
+ * @subpackage file_manager
+ *
+ * @author Hashem Gamal Ahmed
+ * @version Wolf 0.7
+ */
+
+return array(
+        'A possible hack attempt was detected.' => 'تم الكشف عن محاولة إختراق.',
+        'Action' => 'تنشيط',
+        'Are you sure you wish to delete?' => 'هل أنت متأكد أنك تريد الحذف؟',
+        'Cancel' => 'إلغاء',
+        'Change mode' => 'تغيير الوضع',
+        'Close' => 'إغلاق',
+        'Create' => 'إنشاء',
+        'Create new directory' => 'إنشاء دليل جديد',
+        'Create new file' => 'إنشاء ملف جديد',
+        'Delete file' => 'مسح الملف',
+        'Directory :name has not been created!' => 'لم يتم إنشاء دليل :name!',
+        'Directory is not writable! File has not been saved!' => 'الدليل غير قابل للكتابة! لم يتم حفظ الملف!',
+        'File' => 'ملف',
+        'File :name has been created with success!' => 'تم إنشاء ملف :name بنجاح!',
+        'File :name has not been created!' => 'لم يتم إنشاء ملف :name!',
+        'File Manager' => 'إدارة الملفات',
+        'File has been saved with success!' => 'تم حفظ الملف بنجاح.',
+        'File has not been uploaded!' => 'لم يتم رفع الملف!',
+        'File is not writable! File has not been saved!' => 'الملف غير قابل للكتابة! لم يتم حفظ الملف!',
+        'File or directory not found!' => 'لم يتم العثور على الملف أو الدليل!',
+        'Files' => 'الملفات',
+        'Modify' => 'تعديل',
+        'Permission denied!' => 'رفض الإذن!',
+        'Permissions' => 'أذونات',
+        'Provides interface to manage files from the administration.' => 'يوفر واجهة لإدارة الملفات من الواجهة.',
+        'Rename' => 'إعادة تسمية',
+        'Save' => 'حفظ',
+        'Save and Continue Editing' => 'حفظ و متابعة التحرير',
+        'Size' => 'حجم',
+        'Upload' => 'رفع',
+        'Upload file' => 'رفع ملف',
+        'You do not have permission to access the requested page!' => 'ليس لديك الإذن للدخول على الصفحة المطلوبة!',
+        'delete file icon' => 'أيقونة مسح الملف',
+        'or' => 'أو',
+        'overwrite it?' => 'هل تود إستبداله؟',
+);
